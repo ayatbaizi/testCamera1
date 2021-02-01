2c6d19a75ba3ccc25458dd0051b33f7b886e86bd
AyatBaiz <AyatBaiz@LAPTOP-GDEEJEHA>
2021-02-01 18:26:25 +0500

    jnghfdrf

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        Image img;
+        Button takePhotoBtn;
+        Button getPhotoBtn;
+        public Page1()
+        {
+           // InitializeComponent();
+            takePhotoBtn = new Button { Text = "Сделать фото" };
+            getPhotoBtn = new Button { Text = "Выбрать фото" };
+            img = new Image();
+            // 
+            // выбор фото
+            getPhotoBtn.Clicked += GetPhotoAsync;
+
+            // съемка фото
+            takePhotoBtn.Clicked += TakePhotoAsync;
+
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    new StackLayout
+                    {
+                         Children = {takePhotoBtn, getPhotoBtn},
+                         Orientation =StackOrientation.Horizontal,
+                         HorizontalOptions = LayoutOptions.CenterAndExpand
+                    },
+                    img
+                }
+            };
+        }
+        async void GetPhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                // выбираем фото
+                var photo = await MediaPicker.PickPhotoAsync();
+                // загружаем в ImageView
+                img.Source = ImageSource.FromFile(photo.FullPath);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        async void TakePhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                {
+                    Title = $"xamarin.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.png"
+                });
+
+                // для примера сохраняем файл в локальном хранилище
+                var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+                using (var stream = await photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(newFile))
+                    await stream.CopyToAsync(newStream);
+
+                // загружаем в ImageView
+                img.Source = ImageSource.FromFile(photo.FullPath);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        async void btn6_Clicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+            {
+                Title = $"xamarin.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.png"
+            });
+            var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+        }
+
+        async void btn7_Clicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.PickPhotoAsync();
+                img.Source = ImageSource.FromFile(photo.FullPath);
+        }
+    }
+}
+
+    
---
+++ App1/App1/filelection.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class filelection : ContentPage
+    {
+        public filelection()
+        {
+            InitializeComponent();
+        }
+
+        private void btn5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
